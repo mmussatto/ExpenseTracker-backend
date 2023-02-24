f614b1b911dd43a6ce065753e82e339bcc7cb34c
mmussatto <murilomussatto@gmail.com>
2023-02-24 17:14:13 -0300

    Create Tags Entity
    Closes #4

---
+++ src/main/java/dev/mmussatto/expensetracker/domain/Tags.java
@@ -0,0 +1,24 @@
+/*
+ * Created by murilo.mussatto on 24/02/2023
+ */
+
+package dev.mmussatto.expensetracker.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Tags {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    private String name;
+
+    private Color color;
+}
