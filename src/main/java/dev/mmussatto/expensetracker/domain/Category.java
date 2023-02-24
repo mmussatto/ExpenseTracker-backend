8840694f2be9e4c6bce29997ddad6a532458f9eb
mmussatto <murilomussatto@gmail.com>
2023-02-24 17:13:48 -0300

    Create Category Entity
    Closes #3

---
+++ src/main/java/dev/mmussatto/expensetracker/domain/Category.java
@@ -0,0 +1,25 @@
+/*
+ * Created by murilo.mussatto on 24/02/2023
+ */
+
+package dev.mmussatto.expensetracker.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    private String name;
+
+    private Color color;
+
+}
