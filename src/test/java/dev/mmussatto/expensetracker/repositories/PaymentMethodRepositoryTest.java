dbb5678b7ca620c6ace4a94f8ef1e877f90b3aad
mmussatto <murilomussatto@gmail.com>
2023-02-28 09:39:11 -0300

    Create Tests for repositories
    Closes #14

---
+++ src/test/java/dev/mmussatto/expensetracker/repositories/PaymentMethodRepositoryTest.java
@@ -0,0 +1,39 @@
+/*
+ * Created by murilo.mussatto on 27/02/2023
+ */
+
+package dev.mmussatto.expensetracker.repositories;
+
+import dev.mmussatto.expensetracker.domain.PaymentMethod;
+import dev.mmussatto.expensetracker.domain.PaymentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class PaymentMethodRepositoryTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    PaymentMethodRepository paymentMethodRepository;
+
+
+    @Test
+    void findByName() {
+        PaymentMethod t1 = new PaymentMethod("Test 1", PaymentType.CASH);
+        testEntityManager.persist(t1);
+
+        PaymentMethod t2 = new PaymentMethod("Test 2", PaymentType.CREDIT_CARD);
+        testEntityManager.persist(t2);
+
+
+        PaymentMethod test = paymentMethodRepository.findByName("Test 1").get();
+
+        assertEquals(t1.getId(), test.getId());
+    }
+}
