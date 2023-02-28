8d1b181dfa7ff5ee45d68afc8e6f8398df33fdea
mmussatto <murilomussatto@gmail.com>
2023-02-28 10:13:27 -0300

    Create TagDTO
    #15

---
+++ src/main/java/dev/mmussatto/expensetracker/api/v1/model/TagDTO.java
@@ -0,0 +1,20 @@
+/*
+ * Created by murilo.mussatto on 28/02/2023
+ */
+
+package dev.mmussatto.expensetracker.api.v1.model;
+
+import dev.mmussatto.expensetracker.domain.Color;
+import dev.mmussatto.expensetracker.domain.Transaction;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class TagDTO {
+
+    private Integer id;
+    private String name;
+    private Color color;
+    private Set<Transaction> transactions;
+}
