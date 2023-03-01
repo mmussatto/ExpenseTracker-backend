0ba584e7a9120121d64b53a35c2691bc5cf4855c
mmussatto <murilomussatto@gmail.com>
2023-03-01 11:28:58 -0300

    Implement DeleteMapping in CategoryController
    #34

--- src/main/java/dev/mmussatto/expensetracker/controllers/CategoryController.java
+++ src/main/java/dev/mmussatto/expensetracker/controllers/CategoryController.java
@@ -8,7 +8,6 @@ import dev.mmussatto.expensetracker.api.model.CategoryDTO;
 import dev.mmussatto.expensetracker.api.model.CategoryListDTO;
 import dev.mmussatto.expensetracker.services.CategoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -43,6 +42,18 @@ public class CategoryController {
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryDTO createNewCategory (@RequestBody CategoryDTO categoryDTO) {
         return categoryService.createNewCategory(categoryDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCategoryById (@PathVariable final Integer id) {
+        categoryService.deleteCategoryById(id);
+    }
+
+    @DeleteMapping("/name/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCategoryById (@PathVariable final String name) {
+        categoryService.deleteCategoryByName(name);
     }
 
 
