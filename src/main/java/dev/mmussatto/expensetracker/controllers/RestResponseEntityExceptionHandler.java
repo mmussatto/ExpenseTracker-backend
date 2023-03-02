9c3ed6d9c05841ba5ce22a48a16703b60bbb007e
mmussatto <murilomussatto@gmail.com>
2023-03-02 11:29:05 -0300

    Update ExceptionHandler to use the custom error response

--- src/main/java/dev/mmussatto/expensetracker/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/dev/mmussatto/expensetracker/controllers/RestResponseEntityExceptionHandler.java
@@ -14,16 +14,42 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+//    @ExceptionHandler({ResourceNotFoundException.class})
+//    public ResponseEntity<Object> handleNotFoundException (Exception exception, WebRequest request) {
+//        return new ResponseEntity<>("Resource Not Found!", new HttpHeaders(), HttpStatus.NOT_FOUND);
+//    }
+
     @ExceptionHandler({ResourceNotFoundException.class})
-    public ResponseEntity<Object> handleNotFoundException (Exception exception, WebRequest request) {
-        return new ResponseEntity<>("Resource Not Found!", new HttpHeaders(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<CustomErrorResponse> handleNotFoundException (Exception exception, WebRequest request) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse();
+        errorResponse.setMessage(exception.getMessage());
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setError(HttpStatus.NOT_FOUND.getReasonPhrase());
+        errorResponse.setPath(request.getContextPath());
+
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
+
+//    @ExceptionHandler({ResourceAlreadyExistsException.class})
+//    public ResponseEntity<Object> handleAlreadyExistsException (Exception exception, WebRequest request) {
+//        return new ResponseEntity<>("Resource Already Exists!\n" + exception.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT);
+//    }
 
     @ExceptionHandler({ResourceAlreadyExistsException.class})
     public ResponseEntity<Object> handleAlreadyExistsException (Exception exception, WebRequest request) {
-        return new ResponseEntity<>("Resource Already Exists!\n" + exception.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT);
+        CustomErrorResponse errorResponse = new CustomErrorResponse();
+        errorResponse.setMessage("Resource already exists!");
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setStatus(HttpStatus.CONFLICT.value());
+        errorResponse.setError(HttpStatus.CONFLICT.getReasonPhrase());
+        errorResponse.setPath(exception.getMessage());
+
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.CONFLICT);
     }
 }
