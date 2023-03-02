4b3549a189ff3fae68be2321dd8db0f9d95b57cf
mmussatto <murilomussatto@gmail.com>
2023-03-02 16:09:10 -0300

    Update ResourceAlreadyExistsException with custom path field.

--- src/main/java/dev/mmussatto/expensetracker/services/exceptions/ResourceAlreadyExistsException.java
+++ src/main/java/dev/mmussatto/expensetracker/services/exceptions/ResourceAlreadyExistsException.java
@@ -4,24 +4,34 @@
 
 package dev.mmussatto.expensetracker.services.exceptions;
 
+import lombok.Getter;
+
+@Getter
 public class ResourceAlreadyExistsException extends RuntimeException{
 
-    public ResourceAlreadyExistsException() {
+    private final String path;
+
+    public ResourceAlreadyExistsException(String path) {
+        this.path = path;
     }
 
-    public ResourceAlreadyExistsException(String message) {
+    public ResourceAlreadyExistsException(String message, String path) {
         super(message);
+        this.path = path;
     }
 
-    public ResourceAlreadyExistsException(String message, Throwable cause) {
+    public ResourceAlreadyExistsException(String message, Throwable cause, String path) {
         super(message, cause);
+        this.path = path;
     }
 
-    public ResourceAlreadyExistsException(Throwable cause) {
+    public ResourceAlreadyExistsException(Throwable cause, String path) {
         super(cause);
+        this.path = path;
     }
 
-    public ResourceAlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public ResourceAlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, String path) {
         super(message, cause, enableSuppression, writableStackTrace);
+        this.path = path;
     }
 }
