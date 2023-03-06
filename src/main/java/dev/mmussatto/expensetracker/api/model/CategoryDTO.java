4898b051dd4020e930ce6daa55a62d20ab1ba8c7
mmussatto <murilomussatto@gmail.com>
2023-03-06 17:21:12 -0300

    Initialize transactions set and create constructor
    #51

--- src/main/java/dev/mmussatto/expensetracker/api/model/CategoryDTO.java
+++ src/main/java/dev/mmussatto/expensetracker/api/model/CategoryDTO.java
@@ -9,11 +9,14 @@ import dev.mmussatto.expensetracker.domain.Color;
 import dev.mmussatto.expensetracker.domain.Transaction;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
 public class CategoryDTO {
     private Integer id;
     @NotNull
@@ -22,7 +25,11 @@ public class CategoryDTO {
     private Color color;
     @ToString.Exclude
     @JsonIgnore
-    private Set<Transaction> transactions;
+    private Set<Transaction> transactions = new HashSet<>();
     private String path;
 
+    public CategoryDTO(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
 }
