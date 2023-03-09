d00c66c9e35df7b6c78dd48b19d9f1fb90575faf
mmussatto <murilomussatto@gmail.com>
2023-03-09 14:50:10 -0300

    Update Exception Handler to use maps

--- src/main/java/dev/mmussatto/expensetracker/controllers/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/dev/mmussatto/expensetracker/controllers/exceptions/RestResponseEntityExceptionHandler.java
@@ -9,51 +9,67 @@ import dev.mmussatto.expensetracker.services.exceptions.ResourceAlreadyExistsExc
 import dev.mmussatto.expensetracker.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ResourceNotFoundException.class})
-    public ResponseEntity<Object> handleNotFoundException (Exception exception, WebRequest request) {
-        CustomErrorResponse errorResponse = new CustomErrorResponse();
-        errorResponse.setMessage(exception.getMessage());
-        errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setError(HttpStatus.NOT_FOUND.getReasonPhrase());
+    public ResponseEntity<Object> handleNotFoundException (ResourceNotFoundException exception, WebRequest request) {
 
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.NOT_FOUND);
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")));
+        responseBody.put("status", HttpStatus.NOT_FOUND.value());
+        responseBody.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
+        responseBody.put("path", ((ServletWebRequest) request).getRequest().getRequestURI());
+        responseBody.put("message", exception.getMessage());
+
+
+        return new ResponseEntity<>(responseBody, new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
 
 
     @ExceptionHandler({ResourceAlreadyExistsException.class})
     public ResponseEntity<Object> handleAlreadyExistsException (ResourceAlreadyExistsException exception, WebRequest request) {
-        CustomErrorResponse errorResponse = new CustomErrorResponse();
-        errorResponse.setMessage(exception.getMessage());
-        errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setStatus(HttpStatus.CONFLICT.value());
-        errorResponse.setError(HttpStatus.CONFLICT.getReasonPhrase());
-        errorResponse.setPath(exception.getPath());
 
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.CONFLICT);
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")));
+        responseBody.put("status", HttpStatus.CONFLICT.value());
+        responseBody.put("error", HttpStatus.CONFLICT.getReasonPhrase());
+        responseBody.put("path", ((ServletWebRequest) request).getRequest().getRequestURI());
+        responseBody.put("message", exception.getMessage());
+        responseBody.put("savedObjectPath", exception.getPathToObject());
+
+        return new ResponseEntity<>(responseBody, new HttpHeaders(), HttpStatus.CONFLICT);
     }
 
 
     @ExceptionHandler(InvalidIdModificationException.class)
     public ResponseEntity<Object> handleInvalidIdModification(InvalidIdModificationException exception, WebRequest request) {
-        CustomErrorResponse errorResponse = new CustomErrorResponse();
-        errorResponse.setMessage("Id property is immutable. Current id: " + exception.getMessage());
-        errorResponse.setTimestamp(LocalDateTime.now());
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setError(HttpStatus.BAD_REQUEST.getReasonPhrase());
-        errorResponse.setPath(exception.getPath());
 
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")));
+        responseBody.put("status", HttpStatus.BAD_REQUEST.value());
+        responseBody.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        responseBody.put("path", ((ServletWebRequest) request).getRequest().getRequestURI());
+
+        return new ResponseEntity<>(responseBody, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
