2e81cb2d3e96824ab729d4dd1bee2a243c55422f
mmussatto <murilomussatto@gmail.com>
2023-03-09 14:49:41 -0300

    Rename path to pathToObject in ResourceAlreadyExistsException

--- src/main/java/dev/mmussatto/expensetracker/services/exceptions/ResourceAlreadyExistsException.java
+++ src/main/java/dev/mmussatto/expensetracker/services/exceptions/ResourceAlreadyExistsException.java
@@ -9,29 +9,29 @@ import lombok.Getter;
 @Getter
 public class ResourceAlreadyExistsException extends RuntimeException{
 
-    private final String path;
+    private final String pathToObject;
 
     public ResourceAlreadyExistsException(String path) {
-        this.path = path;
+        this.pathToObject = path;
     }
 
     public ResourceAlreadyExistsException(String message, String path) {
         super(message);
-        this.path = path;
+        this.pathToObject = path;
     }
 
     public ResourceAlreadyExistsException(String message, Throwable cause, String path) {
         super(message, cause);
-        this.path = path;
+        this.pathToObject = path;
     }
 
     public ResourceAlreadyExistsException(Throwable cause, String path) {
         super(cause);
-        this.path = path;
+        this.pathToObject = path;
     }
 
     public ResourceAlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, String path) {
         super(message, cause, enableSuppression, writableStackTrace);
-        this.path = path;
+        this.pathToObject = path;
     }
 }
