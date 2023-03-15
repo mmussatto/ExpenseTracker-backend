bb8474c019ee7fcb34ff01d98569b5016165797d
mmussatto <murilomussatto@gmail.com>
2023-03-15 18:06:26 -0300

    Create tests for Vendor controller
    #67

---
+++ src/test/java/dev/mmussatto/expensetracker/controllers/VendorControllerTest.java
@@ -0,0 +1,47 @@
+/*
+ * Created by murilo.mussatto on 15/03/2023
+ */
+
+package dev.mmussatto.expensetracker.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VendorControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllVendors() {
+    }
+
+    @Test
+    void getVendorById() {
+    }
+
+    @Test
+    void testGetVendorById() {
+    }
+
+    @Test
+    void createNewVendor() {
+    }
+
+    @Test
+    void updateVendorById() {
+    }
+
+    @Test
+    void patchVendorById() {
+    }
+
+    @Test
+    void deleteVendorById() {
+    }
+
+    @Test
+    void getVendorTransactionsById() {
+    }
+}
