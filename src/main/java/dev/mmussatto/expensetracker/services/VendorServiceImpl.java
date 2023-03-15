2ab9738313646f20ae57e16068dc778af123cadb
mmussatto <murilomussatto@gmail.com>
2023-03-15 16:01:19 -0300

    Implement functions in vendor service
    #27

--- src/main/java/dev/mmussatto/expensetracker/services/VendorServiceImpl.java
+++ src/main/java/dev/mmussatto/expensetracker/services/VendorServiceImpl.java
@@ -4,6 +4,8 @@
 
 package dev.mmussatto.expensetracker.services;
 
+import dev.mmussatto.expensetracker.domain.OnlineStore;
+import dev.mmussatto.expensetracker.domain.PhysicalStore;
 import dev.mmussatto.expensetracker.domain.Transaction;
 import dev.mmussatto.expensetracker.domain.Vendor;
 import dev.mmussatto.expensetracker.repositories.VendorRepository;
@@ -49,22 +51,62 @@ public class VendorServiceImpl<V extends Vendor> implements VendorService<V> {
 
     @Override
     public V updateVendorById(Integer id, V vendor) {
-        return null;
+
+        vendorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Vendor %d not found!", id)));
+
+        checkIfNameIsAlreadyInUse(vendor);
+
+        vendor.setId(id);
+
+        return vendorRepository.save(vendor);
     }
 
     @Override
     public V patchVendorById(Integer id, V vendor) {
-        return null;
+        return vendorRepository.findById(id).map(savedVendor -> {
+
+            if (vendor.getName() != null) {
+                checkIfNameIsAlreadyInUse(vendor);
+                savedVendor.setName(vendor.getName());
+            }
+
+            if (vendor instanceof PhysicalStore
+                    && ((PhysicalStore) vendor).getAddress() != null
+                    && savedVendor instanceof PhysicalStore) {
+
+                ((PhysicalStore) savedVendor).setAddress(((PhysicalStore) vendor).getAddress());
+            }
+
+            if (vendor instanceof OnlineStore
+                    && ((OnlineStore) vendor).getUrl() != null
+                    && savedVendor instanceof OnlineStore) {
+
+                ((OnlineStore) savedVendor).setUrl(((OnlineStore) vendor).getUrl());
+            }
+
+            if (vendor.getTransactions() != null && vendor.getTransactions().size() != 0)
+                savedVendor.setTransactions(vendor.getTransactions());
+
+            return vendorRepository.save(vendor);
+        }).orElseThrow(() -> new ResourceNotFoundException(String.format("Vendor %d not found!", id)));
     }
 
     @Override
     public void deleteVendorById(Integer id) {
 
+        vendorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Vendor %d not found!", id)));
+
+        vendorRepository.deleteById(id);
     }
 
     @Override
     public Set<Transaction> getTransactionsById(Integer id) {
-        return null;
+        Vendor savedVendor =  vendorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Vendor %d not found!", id)));
+
+        return savedVendor.getTransactions();
     }
 
 
