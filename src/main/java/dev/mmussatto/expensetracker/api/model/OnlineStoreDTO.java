840c82814e1aba79adb6be43a44399a121ad58d8
mmussatto <murilomussatto@gmail.com>
2023-03-15 17:28:36 -0300

    Add validation constraints to DTOs
    #37

--- src/main/java/dev/mmussatto/expensetracker/api/model/OnlineStoreDTO.java
+++ src/main/java/dev/mmussatto/expensetracker/api/model/OnlineStoreDTO.java
@@ -4,11 +4,14 @@
 
 package dev.mmussatto.expensetracker.api.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class OnlineStoreDTO extends VendorDTO {
+
+    @NotNull(message = "url must not be null",  groups = VendorDTO.allFieldsValidation.class)
     private String url;
 }
--- src/main/java/dev/mmussatto/expensetracker/api/model/PhysicalStoreDTO.java
+++ src/main/java/dev/mmussatto/expensetracker/api/model/PhysicalStoreDTO.java
@@ -4,11 +4,14 @@
 
 package dev.mmussatto.expensetracker.api.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class PhysicalStoreDTO extends VendorDTO {
+
+    @NotNull(message = "address must not be null",  groups = VendorDTO.allFieldsValidation.class)
     private String address;
 }
