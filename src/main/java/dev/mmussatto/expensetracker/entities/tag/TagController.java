8959ebe7d83f54d1140a4968dbbb149d9b17a8bc
mmussatto <murilomussatto@gmail.com>
2023-03-30 10:03:01 -0300

    Add swagger documentation to tag controller
    #83

--- src/main/java/dev/mmussatto/expensetracker/entities/tag/TagController.java
+++ src/main/java/dev/mmussatto/expensetracker/entities/tag/TagController.java
@@ -8,6 +8,10 @@ import dev.mmussatto.expensetracker.entities.helpers.ListDTO;
 import dev.mmussatto.expensetracker.entities.transaction.Transaction;
 import dev.mmussatto.expensetracker.entities.transaction.TransactionDTO;
 import dev.mmussatto.expensetracker.entities.transaction.TransactionMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@io.swagger.v3.oas.annotations.tags.Tag(name = "Tags", description = "CRUD API for Tag entity")
 @Validated
 @RestController
 @RequestMapping("/api/tags")
@@ -29,6 +34,12 @@ public class TagController {
         this.tagMapper = tagMapper;
     }
 
+
+
+    @Operation(summary = "Get all tags")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the tags", useReturnTypeSchema = true)
+    })
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public ListDTO<TagDTO> getAllTags () {
@@ -38,18 +49,39 @@ public class TagController {
                 .collect(Collectors.toList()));
     }
 
+
+    @Operation(summary = "Get a tag by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the tag", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Tag not found", content = @Content)
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public TagDTO getTagById (@PathVariable final Integer id) {
         return convertToDTO(tagService.getTagById(id));
     }
 
+
+    @Operation(summary = "Get a tag by its name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the tag", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Invalid name supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Tag not found", content = @Content)
+    })
     @GetMapping("/name/{name}")
     @ResponseStatus(HttpStatus.OK)
     public TagDTO getTagByName (@PathVariable final String name) {
         return convertToDTO(tagService.getTagByName(name));
     }
 
+
+    @Operation(summary = "Create tag")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Tag created", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Invalid body supplied", content = @Content),
+            @ApiResponse(responseCode = "409", description = "Name supplied is already in use", content = @Content)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Validated(TagDTO.allFieldsValidation.class)
@@ -58,6 +90,14 @@ public class TagController {
         return convertToDTO(tagService.createNewTag(entity));
     }
 
+
+    @Operation(summary = "Update tag")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tag updated", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Invalid body supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Tag not found", content = @Content),
+            @ApiResponse(responseCode = "409", description = "Name supplied is already in use", content = @Content)
+    })
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @Validated(TagDTO.allFieldsValidation.class)
@@ -66,6 +106,14 @@ public class TagController {
         return convertToDTO(tagService.updateTagById(id, entity));
     }
 
+
+    @Operation(summary = "Patch tag")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tag patched", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Invalid body supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Tag not found", content = @Content),
+            @ApiResponse(responseCode = "409", description = "Name supplied is already in use", content = @Content)
+    })
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @Validated(TagDTO.onlyIdValidation.class)
@@ -74,12 +122,25 @@ public class TagController {
         return convertToDTO(tagService.patchTagById(id, entity));
     }
 
+
+    @Operation(summary = "Delete tag")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Tag deleted", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = "Tag not found", content = @Content)
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTagById (@PathVariable final Integer id) {
         tagService.deleteTagById(id);
     }
 
+
+    //Transactions
+    @Operation(summary = "Get all transactions from a tag")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the tag's transactions", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = "Tag not found", content = @Content)
+    })
     @GetMapping("/{id}/transactions")
     @ResponseStatus(HttpStatus.OK)
     public ListDTO<TransactionDTO> getPaymentMethodTransactionsById (@PathVariable final Integer id) {
@@ -94,6 +155,8 @@ public class TagController {
                 }).collect(Collectors.toList()));
     }
 
+
+    //Mappers
     private TagDTO convertToDTO (Tag entity) {
         TagDTO tagDTO = tagMapper.convertToDTO(entity);
         tagDTO.setPath("/api/tags/" + tagDTO.getId());
