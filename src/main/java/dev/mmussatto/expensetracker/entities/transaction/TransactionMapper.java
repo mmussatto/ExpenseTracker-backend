1dfd8162e8039e0979281f4868531dd8950d78da
mmussatto <murilomussatto@gmail.com>
2023-04-04 15:26:43 -0300

    Add RequestTransactionDTO to TransactionMapper
    #84
    
    Add function that will create a Transaction entity from the RequestDTO. It will also map the vendor to a defaultVendor

--- src/main/java/dev/mmussatto/expensetracker/entities/transaction/TransactionMapper.java
+++ src/main/java/dev/mmussatto/expensetracker/entities/transaction/TransactionMapper.java
@@ -4,7 +4,14 @@
 
 package dev.mmussatto.expensetracker.entities.transaction;
 
+import dev.mmussatto.expensetracker.entities.category.Category;
+import dev.mmussatto.expensetracker.entities.paymentmethod.PaymentMethod;
+import dev.mmussatto.expensetracker.entities.tag.Tag;
+import dev.mmussatto.expensetracker.entities.vendor.Vendor;
+import dev.mmussatto.expensetracker.entities.vendor.defaultvendor.DefaultVendor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -15,4 +22,29 @@ public interface TransactionMapper {
     TransactionDTO convertToDTO(Transaction transaction);
 
     Transaction convertToEntity(TransactionDTO transactionDTO);
+
+
+    //Request Mappers
+    @Mapping(target = "category", source = "source.categoryId")
+    @Mapping(target = "paymentMethod", source = "source.paymentMethodId")
+    @Mapping(target = "vendor", source = "source", qualifiedByName="mapVendorWithIdAndType")
+    @Mapping(target = "tags", source = "source.tagIds")
+    Transaction convertRequestToEntity (RequestTransactionDTO source);
+
+
+    //Map objects inside request
+    @Mapping(target = "id", source = "categoryId")
+    Category mapCategory(Integer categoryId);
+
+    @Mapping(target = "id", source = "paymentMethodId")
+    PaymentMethod mapPaymentMethod (Integer paymentMethodId);
+
+    @Mapping(target = "id", source = "tagId")
+    Tag mapTags (Integer tagId);
+
+    @Named("mapVendorWithIdAndType")
+    default Vendor mapVendor (RequestTransactionDTO source) {
+        return source.getVendorId() == null ? null : new DefaultVendor(source.getVendorId());
+    }
+
 }
